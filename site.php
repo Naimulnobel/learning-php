782b698ffdc4cda5cf0f1697f5740bde5bb7a947
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-05-26 06:32:30 +0600

    Add files via upload

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo 'hello<br>';
+      $name="nobel";
+      echo $name;
+      $animal=['dog','cat','tiger'];
+      print_r($animal);
+      echo $animal[0];
+     ?>
+
+  </body>
+</html>
